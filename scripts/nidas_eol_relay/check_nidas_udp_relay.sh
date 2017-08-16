33ad052f5bc1dd8c7be6078b39a7f9a41ae24566
Gary Granger <granger@ucar.edu>
2017-08-16 15:55:44 -0600

    revert to a single relay on port 30010

--- scripts/nidas_eol_relay/check_nidas_udp_relay.sh
+++ scripts/nidas_eol_relay/check_nidas_udp_relay.sh
@@ -7,21 +7,11 @@ script=$0
 script=${script##*/}
 logdir=$HOME/nidas_eol_relay
 
-# museum UDP stream bound for barolo
 proc="nidas_udp_relay -h artse.txt -u 30010"
 
 if ! pgrep -f "$proc" > /dev/null; then
     (cd "$logdir"
 	echo "`date`: ***** $proc not running, starting it..."
 	exec $proc) >> $logdir/nidas_eol_relay.log 2>&1
-fi
-
-# airport UDP stream bound for barolo and flux2
-proc="nidas_udp_relay -h artse.txt -u 30011"
-
-if ! pgrep -f "$proc" > /dev/null; then
-    (cd "$logdir"
-	echo "`date`: ***** $proc not running, starting it..."
-	exec $proc) >> $logdir/nidas_eol_relay_2.log 2>&1
 fi
 
