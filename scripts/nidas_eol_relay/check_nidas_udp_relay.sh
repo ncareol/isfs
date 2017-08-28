f09de7f4209bfce6cfa92272f7ef4619217c916e
Gary Granger <granger@ucar.edu>
2017-08-28 16:36:45 -0600

    shorten command to match pgrep, add sysv init ops start/stop/status

--- scripts/nidas_eol_relay/check_nidas_udp_relay.sh
+++ scripts/nidas_eol_relay/check_nidas_udp_relay.sh
@@ -7,11 +7,58 @@ script=$0
 script=${script##*/}
 logdir=$HOME/nidas_eol_relay
 
+export NTOP=/home/granger/opt/nidas
+export LD_LIBRARY_PATH="$NTOP/lib64"
+export PATH="$NTOP/bin:${PATH}"
 proc="nidas_udp_relay -h artse.txt -u 30010"
 
-if ! pgrep -f "$proc" > /dev/null; then
-    (cd "$logdir"
-	echo "`date`: ***** $proc not running, starting it..."
-	exec $proc) >> $logdir/nidas_eol_relay.log 2>&1
+debug=0
+if [ "$1" == debug ]; then
+    debug=1
+    shift
 fi
 
+if [ $debug -ne 1 ]; then
+    exec >> $logdir/nidas_eol_relay.log 2>&1
+fi
+
+start()
+{
+    if ! pgrep -f "$proc" > /dev/null; then
+	(cd "$logdir"
+	    echo "`date`: ***** $proc not running, starting it..."
+	    exec $proc) &
+    fi
+}
+
+stop()
+{
+    pkill -f "$proc"
+}
+
+
+case "$1" in
+
+    start)
+	start
+	;;
+
+    stop)
+	stop
+	;;
+
+    status)
+	pid=`pgrep -f "$proc"`
+	if [ -n "$pid" ]; then
+	    (set -x ; ps ufp "$pid")
+	else
+	    echo "Not running."
+	fi
+	;;
+
+    *)
+	echo "Usage: $0 [debug] {start|stop|status}"
+	exit 1
+	;;
+
+esac
